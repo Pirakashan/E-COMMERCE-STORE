7b4b799fa2add671c36e1f3472e8cc33be67902a
Pirakashan <mpirakashan@gmail.com>
2025-12-16 23:52:50 +0530

    update

--- frontend/src/pages/PurchaseSuccess.jsx
+++ frontend/src/pages/PurchaseSuccess.jsx
@@ -5,10 +5,6 @@ import { useCartStore } from "../stores/useCartStore";
 import axios from "../lib/axios";
 import Confetti from "react-confetti";
 
-const PurchaseSuccessPage = () => {
-	const [isProcessing, setIsProcessing] = useState(true);
-	const { clearCart } = useCartStore();
-	const [error, setError] = useState(null);
 
 	useEffect(() => {
 		const handleCheckoutSuccess = async (sessionId) => {
